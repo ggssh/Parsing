53f6135e8015facaf2d1c72e09f990665659e0e1
nomanker <nomankes@gmail.com>
2021-03-03 10:58:22 +0800

    提取左公因子和消除左递归完成

--- src/com/gogogo/parsing/main.java
+++ src/com/gogogo/parsing/main.java
@@ -7,4 +7,11 @@ package com.gogogo.parsing;
  * @Date: 2021/2/24 0024 20:34
  */
 public class main {
+    public static void main(String[] args) {
+        Grammar grammar = new Grammar();
+        grammar.setProductions("grammar_input.txt");
+        grammar.setVN("grammar_input.txt");
+        grammar.setVT();
+        grammar.leftFactoring();
+    }
 }
